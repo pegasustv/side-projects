a4c93e565eefe29954219938cbc818a2ce42ebef
Brandon Bridges <brndnbrdgs1@gmail.com>
2019-04-22 16:28:58 -0400

    Updates

---
+++ Cats.cpp
@@ -0,0 +1,19 @@
+/*
+   File: Cats.cpp
+   Created by: Brandon Bridges
+   Creation Date: 07/03/2019
+   Synopsis: Something about cats
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+        char food[] = "bacon and eggs, asparagus, broccoli, and coffee with heavy cream";
+
+        cout << "your should feed your cat: " << food;
+
+        return 0;
+}
---
+++ Poems.cpp
@@ -0,0 +1,69 @@
+/*
+   File: Poems.cpp
+   Created by: Brandon Bridges
+   Creation Date: 07/03/2019
+   Synopsis: Some nice poems
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+        cout << "I don't have regret for the things that I did" << endl;
+        cout << "The stupid decisions I made as a kid" << endl;
+        cout << "Not always the smartest" << endl;
+        cout << "not always to plan" << endl;
+        cout << "But still, as I made them, they made me a man." << endl;
+        cout << endl;
+        cout << "Perhaps, if I'd waited, it might have been fine" << endl;
+        cout << "Or fated for futures all finer than mine." << endl;
+        cout << "It might have brought all that I'd wanted about" << endl;
+        cout << "But what if it didn't?" << endl;
+        cout << "I'd never find out." << endl;
+        cout << endl;
+        cout << "Perhaps I could better have sheltered myself" << endl;
+        cout << "But life isn't meant to be lived on a shelf." << endl;
+        cout << "Though some were a failure," << endl;
+        cout << "and some were a fall" << endl;
+        cout << endl;
+        cout << "I'd only regret doing nothing at all." << endl;
+
+        cout << endl << endl;
+
+        cout << "From time to time," << endl;
+        cout << "I think of then." << endl;
+        cout << "I turn my gaze before again." << endl;
+        cout << "And though the sights are all a blur," << endl;
+        cout << "I think of her," << endl;
+        cout << "and who we were." << endl;
+        cout << endl;
+        cout << "From time to time," << endl;
+        cout << "I think of then." << endl;
+        cout << "I think of who and why and when" << endl;
+        cout << "And where she is," << endl;
+        cout << "and what could be." << endl;
+        cout << endl;
+        cout << "I wonder if she thinks of me." << endl;
+
+        cout << endl << endl;
+
+        cout << "Remember how we used to smile" << endl;
+        cout << "To share a foolish thought?" << endl;
+        cout << "To simply wait and walk a while?" << endl;
+        cout << "To talk of all, and nought?" << endl;
+        cout <, endl;
+        cout << "We'd spend our days and nights as two," << endl;
+        cout << "Content to stare above." << endl;
+        cout << "I always felt the same with you" << endl;
+        cout << "It seemed a sort of love." << endl;
+        cout << endl;
+        cout << "And so we made it something more" << endl;
+        cout << "But something came to end." << endl;
+        cout << "We never found what came before." << endl;
+        cout << endl;
+        cout << "I miss my finest friend." << endl;
+
+        return 0;
+}
